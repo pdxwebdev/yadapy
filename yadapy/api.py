287503b874646c08c4a69e0e23b6ca3e8dfff214
Matt Vogel <matt@portlandwebdev.com>
2014-09-21 13:25:30 -0700

    adding api daemon

---
+++ yadapy/api.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+import json, logging, re
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+from nodecommunicator import NodeCommunicator
+from db.mongodb.manager import YadaServer
+from db.mongodb.node import Node
+from api.node import MongoApi
+
+from lib.crypt import decrypt
+
+### Protocol Implementation
+Node.host = 'yadaproject.com'
+Node.port = 27021
+# This is just about the simplest possible protocol
+YadaServer._data = {}
+YadaServer._data['public_key'] = '84ce10c5-5970-4007-92a7-c1f00f0329c5'
+ys = YadaServer()
+nodeComm = NodeCommunicator(ys)
+
+def loadInboundJson(inbound):
+    jsonDict = {}
+    try:
+        
+        jsonDict = json.loads(inbound)
+        if type(jsonDict['data']) == type("") or type(jsonDict['data']) == type(u""):
+            jsonDict['data'] = jsonDict['data'].replace(' ', '+')
+    except:
+        logging.debug('loadInboundJson error in parsing json')
+    return jsonDict
+
+def getProfileFromInbound(jsonDict):
+    try:
+        return Node.col.find({'public_key':jsonDict['public_key']},{"public_key": 1,"private_key": 1,"modified": 1, 'data.friends': 1, 'data.identity.name': 1, 'data.identity.avatar': 1, 'data.identity.ip_address': 1})[0]
+    except:
+        return None
+
+class Echo(Protocol):
+            
+    def dataReceived(self, inbound):
+        """
+        As soon as any data is received, write it back.
+        """
+        node = YadaServer()
+        nodeComm = NodeCommunicator(node)
+        mongoapi = MongoApi(nodeComm)
+        try:
+            jsonDict = loadInboundJson(inbound)
+            data = getProfileFromInbound(jsonDict)
+            decrypted = {}
+            if data:
+                try:
+                    decrypted.update(json.loads(decrypt(data['private_key'], data['private_key'], jsonDict['data'])))
+                except:
+                    decrypted = jsonDict['data']
+            
+            response = getattr(mongoapi, jsonDict['METHOD'])(data, decrypted)
+            jsonResponse = json.dumps(response)
+            self.transport.write(jsonResponse)
+        except Exception as ex:
+            logging.exception(ex)
+            self.transport.write('Input not valid.')
+        self.transport.loseConnection()
+
+
+def main():
+    f = Factory()
+    f.protocol = Echo
+    reactor.listenTCP(8900, f)
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
