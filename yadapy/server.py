9070b4a9cfd5233e7fa58ac2f58ac6a997366585
Matt Vogel <matt@portlandwebdev.com>
2014-10-15 10:30:53 +0000

    adding autobahn websockets

--- yadapy/server.py
+++ yadapy/server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
-import json, re, logging, os, sys
+import json, re, logging, os, sys, hashlib, base64, traceback
 from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
 from nodecommunicator import NodeCommunicator
@@ -26,34 +27,56 @@ ys = YadaServer()
 nodeComm = NodeCommunicator(ys)
 mongoapi = MongoApi(nodeComm)
 
-class Echo(Protocol):
-    def connectionMade(self):
-        f = open('/home/phablet/yadaserver.log', 'w')
-        f.write('connected2')
+#class Echo(Protocol):
+class MyServerProtocol(WebSocketServerProtocol):    
+    #def connectionMade(self):
+    def onConnect(self, request):
+        f = open('/home/phablet/yadaserver.log', 'a')
+        f.write('connected2\n')
         f.close()
-	self.transport.write("Welcome!")
-	call (['python', '/home/phablet/notify.py'])
-            
-    def dataReceived(self, inbound):
+	#call (['python', '/home/phablet/notify.py'])
+         
+    #def dataReceived(self, inbound):
+    def onMessage(self, inbound, isBinary):
         """
         As soon as any data is received, write it back.
         """
-        try:
-	    f = open('/home/phablet/yadaserver.log', 'w')
-            f.write(inbound)
+	f = open('/home/phablet/yadaserver.log', 'a')
+	if 'websocket' in inbound:
+            headers = dict(re.findall(r"(?P<name>.*?): (?P<value>.*?)\r\n", inbound))
+	    key = headers['Sec-WebSocket-Key']
+	    f.write('|---|%s|---|\n' % key)
+	    newhash = base64.b64encode(hashlib.sha1(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11').digest())
+	    
+	    self.transport.write("""HTTP/1.1 101 Switching Protocols
+Upgrade: websocket
+Connection: Upgrade
+Sec-WebSocket-Accept: %s
+
+
+
+""" % newhash)
+	    return
+	try:
+            f.write('|---|%s|---|\n' % inbound)
+	    inbound = json.loads(base64.b64decode(inbound))
+	    f.write(json.dumps(inbound))
 	    response = nodeComm.handlePacket(inbound)
             returnData = json.dumps(response)
             
             self.transport.write(returnData)
         except:
-	    f.write('got something but failed')
+	    exc_type, exc_value, exc_traceback = sys.exc_info()
+	    lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+    	    f.write(''.join('!! ' + line for line in lines))
             self.transport.write('Input not valid.')
             f.close()
-        self.transport.loseConnection()
 
 def main():
-    f = Factory()
-    f.protocol = Echo
+    #f = Factory()
+    #f.protocol = Echo
+    f = WebSocketServerFactory("ws://localhost:8901", debug = False)
+    f.protocol = MyServerProtocol
     reactor.listenTCP(8901, f)
     reactor.run()
 
