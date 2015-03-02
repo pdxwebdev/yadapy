740045f3d2e9b8228305fbcfde62b733b91b6342
Matt Vogel <matt@portlandwebdev.com>
2015-03-01 21:13:02 -0800

    sigmajs files

--- documentation/server.py
+++ documentation/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from wtforms import Form, TextAreaField, TextField
 from pymongo import Connection
 import pymongo
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -11,6 +12,21 @@ class StepForm(Form):
     obj = TextAreaField('Object')
     ref = TextField('Parent')
     order = TextField('Order')
+
+@app.route('/')
+def docs():
+    con = Connection('localhost')
+    steps = con.standard.docs.find().sort("order", pymongo.ASCENDING)
+    steps_dict = {}
+    for step in steps:
+        if step['ref']:
+            if 'substeps' not in steps_dict[step['ref']]:
+                steps_dict[step['ref']]['substeps'] = []
+            steps_dict[step['ref']]['substeps'].append(step)
+        else:
+            steps_dict[str(step['_id'])] = step
+    newlist = sorted([step for x, step in steps_dict.items()], key=itemgetter('order')) 
+    return render_template('docs.html', steps=newlist)
 
 @app.route('/edit/', methods=['GET', 'POST'])
 def edit():
@@ -27,8 +43,16 @@ def edit():
     elif request.method == 'GET':
         form = StepForm()
         steps = con.standard.docs.find().sort("order", pymongo.ASCENDING)
-        
-        return render_template('edit.html', form=form, steps=steps)
+        steps_dict = {}
+        for step in steps:
+            if step['ref']:
+                if 'substeps' not in steps_dict[step['ref']]:
+                    steps_dict[step['ref']]['substeps'] = []
+                steps_dict[step['ref']]['substeps'].append(step)
+            else:
+                steps_dict[str(step['_id'])] = step
+        newlist = sorted([step for x, step in steps_dict.items()], key=itemgetter('order')) 
+        return render_template('edit.html', form=form, steps=newlist)
 
 if __name__ == '__main__':
     app.run(debug=True)
